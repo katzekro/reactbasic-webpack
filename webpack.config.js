634afff4baa98d2563ac8ef87892c7faf45ddc07
Leo <kansuke.dev@gmail.com>
2021-03-11 00:22:39 -0600

    fix bug :D

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPatch: '/'
+        publicPath: '/'
     },
 
     resolve:{
